3e3bd8dc62427dc5f74fd7cd35db6a76c6e43370
jthscat <jthscat987654@gmail.com>
2021-12-17 21:49:37 +0800

    test

--- hw2-2.js
+++
@@ -1,9 +0,0 @@
-import * as THREE from "https://threejs.org/build/three.module.js";
-
-export function makeRect(rect) {
-  let Rect = {};
-  Rect.max = car.localToWorld(new THREE.Vector3(4.5, 0, 2));
-  Rect.min = car.localToWorld(new THREE.Vector3(-4.5, 0, -2));
-  Rect.px = car.localToWorld(new THREE.Vector3(1, 0, 0)).sub(car.position);
-  return Rect;
-}
