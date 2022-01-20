40e1ef84e335e0fdcab67558a75d7b7e83f28d0e
jthscat <jthscat987654@gmail.com>
2022-01-21 01:28:01 +0800

    final

--- hw5/mainv2.js
+++ hw5/mainv2.js
@@ -81,7 +81,7 @@ function init() {
 		].join("\n")
     });
 	
-	var geometry = new TeapotGeometry (7);
+	var geometry = new TeapotGeometry (10);
 	    geometry.computeBoundingSphere();
 	    imposterRadius = geometry.boundingSphere.radius;
 		
