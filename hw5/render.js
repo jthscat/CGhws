66da022a7eb955e8f503214c4a6cee98c859051d
jthscat <jthscat987654@gmail.com>
2022-01-20 22:32:52 +0800

    test

---
+++ hw5/render.js
@@ -0,0 +1,13 @@
+import {scene,camera,renderer} from "./main.js";
+
+function render() {
+    renderer.render(scene, camera);
+}
+
+function onWindowResize() {
+  camera.aspect = window.innerWidth / window.innerHeight;
+  camera.updateProjectionMatrix();
+  renderer.setSize(window.innerWidth, window.innerHeight);
+}
+
+export {onWindowResize,render};
